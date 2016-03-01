59e4138c1f2b1e65afc2a4c26da5a29aeabe842b
elf Pavlik <perpetual-tripper@wwelves.org>
2016-03-01 15:11:19 -0600

    conform to standardjs style

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-/* jshint asi: true */
-
 var request = require('then-request')
 var uriTemplates = require('uri-templates')
 var _ = require('lodash')
 
-var api =  {
+var api = {
   maxPageSize: 100,
   templates: {
-    collection: "?per_page={items}",
-    paging: "?per_page={items}&page={page}"
+    collection: '?per_page={items}',
+    paging: '?per_page={items}&page={page}'
   }
 }
 
 // '<https://api.github.com/user/876431/followers?per_page=100&page=2> rel="next", <https://api.github.com/user/876431/followers?per_page=100&page=2> rel="last"'
-function parseNumberOfPages(response) {
-  if(response.headers.link) {
-    //and matches rel="last"
+function parseNumberOfPages (response) {
+  if (response.headers.link) {
+    // and matches rel="last"
     return Number(response.headers.link.match(/.*page=(.*)>; rel="last"/)[1])
   } else {
     return 1
   }
 }
 
-function fetchResource(url, options) {
+function fetchResource (url, options) {
   return request('GET', url, options)
-    .then(function(res) {
-      if(res.statusCode === 200) {
+    .then(function (res) {
+      if (res.statusCode === 200) {
         return res.body.toString()
       } else {
         return Promise.reject(res.body.toString())
@@ -33,16 +31,16 @@ function fetchResource(url, options) {
     })
 }
 
-function fetchCollection(url, numberOfPages, options) {
-  return Promise.all(_.range(1, numberOfPages + 1).map(function(index) {
+function fetchCollection (url, numberOfPages, options) {
+  return Promise.all(_.range(1, numberOfPages + 1).map(function (index) {
     var tmpl = uriTemplates(api.templates.paging)
     var pageUrl = url + tmpl.fill({ items: api.maxPageSize, page: index })
     return request('GET', pageUrl, options)
-      .then(function(res) {
+      .then(function (res) {
         if (res.statusCode === 200) return JSON.parse(res.body.toString())
         else return Promise.reject(res.body.toString())
       })
-  })).then(function(data) {
+  })).then(function (data) {
     return JSON.stringify(_.flatten(data))
   })
 }
@@ -53,14 +51,14 @@ module.exports = function (url, options) {
       'User-Agent': 'https://github.com/then/then-request'
     }
   }
-  if(options && options.token) {
-    opt.headers['Authorization'] = "token " + options.token
+  if (options && options.token) {
+    opt.headers['Authorization'] = 'token ' + options.token
   }
   var template = uriTemplates(api.templates.collection)
   return request('HEAD', url + template.fill({ items: api.maxPageSize }), opt)
-    .then(function(res) {
+    .then(function (res) {
       var numberOfPages = parseNumberOfPages(res)
-      if( numberOfPages > 1) {
+      if (numberOfPages > 1) {
         return fetchCollection(url, numberOfPages, opt)
       } else {
         return fetchResource(url, opt)
